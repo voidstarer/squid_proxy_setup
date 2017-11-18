4fc67d18fa6b12ee13a5bd40314ef3c6169698e5
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-11-19 04:46:03 +0530

    identifying environments

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python2
+
+
 import os
 
 def main():
+    '''
+    Main() : main functionality of the code
+    '''
+    if os.name is 'Posix' or 'posix':
+        print "Hi " + os.getenv('USER') + ", this looks like a POSIX Shell"
+    else:
+        print "Sorry, Not a Posix Shell"
+
+
+
+if __name__=="__main__":
+    main()
