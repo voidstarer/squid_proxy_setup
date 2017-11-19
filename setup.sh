087f3967c57fb013dcae6d4e74c6597626244931
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-11-20 01:35:47 +0530

    BASH Support for cleanup and install

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-
+apt-get update && upgrade
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 
-sudo apt-get -y install iptables-persistent
+apt-get -y install iptables-persistent git
 
+# get the repo to install and set-up
+git clone https://github.com/light-bringer/python-shell
+cd python-shell
 python setup.py
+
+# after setup is done, uninstall git and cleanup
+apt-get remove git
+cd ..
+rm -rf python-shell
