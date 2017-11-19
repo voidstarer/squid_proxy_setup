0536b6b34e5a2183325dcd89b404c31e83142a7a
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-11-20 01:51:13 +0530

    fixing issues with autoprompt

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-apt-get update && upgrade
+apt-get update
+apt-get -y upgrade
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 
 apt-get -y install iptables-persistent git
 
+echo "Doing my task.."
 # get the repo to install and set-up
 git clone https://github.com/light-bringer/python-shell
 cd python-shell
 python setup.py
 
 # after setup is done, uninstall git and cleanup
-apt-get remove git
+echo "Cleaning UP..."
+apt-get -y remove git
 cd ..
 rm -rf python-shell
+echo "Done......!"
