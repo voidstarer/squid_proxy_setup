dadea22db26b3676e1e0555141221b456f1338ac
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-11-22 04:10:31 +0530

    fixed ipv6 auto entry

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get -y upgrade
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
 apt-get -y install iptables-persistent git
 
